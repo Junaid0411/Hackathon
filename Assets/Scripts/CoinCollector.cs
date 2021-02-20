213fcb37c0a6c1f7f6a44670fe3da1780bdb4942
Junaid <191033551@aston.ac.uk>
2021-02-20 22:57:46 +0000

    Updated game

---
+++ Assets/Scripts/CoinCollector.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class CoinCollector : MonoBehaviour
+{
+    private float coins = 0;
+
+    private void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.tag == "Coins") {
+            Destroy(other.gameObject);
+        }
+    }
+}
