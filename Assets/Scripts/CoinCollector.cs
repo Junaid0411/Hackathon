0f9bfbbd7edcd8b411c7f0fe6c504af157cb502c
Junaid <191033551@aston.ac.uk>
2021-02-21 07:06:14 +0000

    Coin Feature updated
    - Coin sound
    - HUD increments
    - 10 coins added

--- Assets/Scripts/CoinCollector.cs
+++ Assets/Scripts/CoinCollector.cs
@@ -1,11 +1,19 @@
 ﻿using UnityEngine;
+using TMPro;
 
 public class CoinCollector : MonoBehaviour
 {
     private float coins = 0;
 
+    public TextMeshProUGUI textCoins;
+    public AudioClip coinsSound;
+
     private void OnTriggerEnter2D(Collider2D other) {
         if (other.transform.tag == "Coins") {
+            coins++;
+
+            textCoins.text = coins.ToString();
+            AudioSource.PlayClipAtPoint(coinsSound, transform.position);
             Destroy(other.gameObject);
         }
     }
